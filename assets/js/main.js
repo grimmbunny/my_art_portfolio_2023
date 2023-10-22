77e4dcd4abefd56c12d5e754f25d1306cb0bcc9e
flavinha <thecutestvillain@gmail.com>
2023-10-22 14:46:04 -0300

    responsive fixes + gtag

--- assets/js/main.js
+++ assets/js/main.js
@@ -225,3 +225,12 @@ hamburger.addEventListener("click", () => {
     removeLabels();
   }
 });
+
+
+/*=============== Google tag (gtag.js) ===============*/
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QY8YQBHZTD');
