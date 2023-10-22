71a46e4bd3e305af22e7033e78d11e80bbaed5b1
flavinha <thecutestvillain@gmail.com>
2023-10-22 14:50:26 -0300

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -228,9 +228,8 @@ hamburger.addEventListener("click", () => {
 
 
 /*=============== Google tag (gtag.js) ===============*/
-
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-QY8YQBHZTD');
+  gtag('config', 'G-WX7LPS41CJ');
